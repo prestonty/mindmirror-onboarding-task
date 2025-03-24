cc396048987b95179518dd620e1d94d7440e120e
Preston Tom-Ying <preston.tomying@gmail.com>
2025-03-24 09:48:23 -0400

    added emotion model

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 """
-STUDENT NAME:
-STUDENT ID:
+STUDENT NAME: Preston Tom-Ying
+STUDENT ID: 21065097
 """
 
 from transformers import pipeline
+import json
 
 # Load your chosen models here
 def load_emotion_model():
     # Research and return a pre-trained emotion detection pipeline
-    pass
+    classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
+    return  classifier
 
 def load_summarization_model():
     # Research and return a pre-trained summarization pipeline
@@ -17,12 +19,20 @@ def load_summarization_model():
 # Process journal entries and return emotion predictions
 def detect_emotions(text_entries, emotion_model):
     # Implement logic to process entries and extract emotions
-    pass
+    emotions = emotion_model(text_entries)
+    
+    return emotions
 
 # Generate summaries for journal entries
 def summarize_entries(entries, summarizer):
     # Implement logic to summarize each entry
     pass
+
+# Use later
+# def read_entries(file_name):
+#     with open(file_name + '.json') as f:
+#         data = json.load(f)
+#     return data
 
 if __name__ == '__main__':
     # Load models
